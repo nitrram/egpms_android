893921fa5245c103e7fdd55bc43ec67cc221751d
Kodovsky <martin.kodovsky@firma.seznam.cz>
2018-09-11 15:29:48 +0200

    config draft

--- app/src/main/java/egpms/lol/wtf/egpms/ControlActivity.kt
+++ app/src/main/java/egpms/lol/wtf/egpms/ControlActivity.kt
@@ -1,12 +1,27 @@
 package egpms.lol.wtf.egpms
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import android.widget.CompoundButton
 import kotlinx.android.synthetic.main.activity_control.*
 
-class ControlActivity : AppCompatActivity() {
+class ControlActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_add -> {
+                // Handle the camera action
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +30,7 @@ class ControlActivity : AppCompatActivity() {
         val i = getStatus()
 
         updateSwitches(i)
+
 
         btn_sock_one.setOnCheckedChangeListener {v,b -> handleClick(v,b) }
         btn_sock_two.setOnCheckedChangeListener{v,b -> handleClick(v,b) }
