18e48d097b0357adce87465b5ad42aceba96e4e5
Martin Kodovský <martin.kodovsky@firma.seznam.cz>
2018-09-13 00:18:12 +0200

    draft sharedPref

--- app/src/main/java/egpms/lol/wtf/egpms/ProfileActivity.kt
+++ app/src/main/java/egpms/lol/wtf/egpms/ProfileActivity.kt
@@ -2,15 +2,46 @@ package egpms.lol.wtf.egpms
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import egpms.lol.wtf.egpms.data.EAction
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import egpms.lol.wtf.egpms.data.Profile
+
 class ProfileActivity : AppCompatActivity() {
+
+    val PREFS_FILENAME = "egpms.lol.wtf.egpms.prefs"
+    val PROFILES = "profiles"
+    var prefs: SharedPreferences? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        prefs = this.getSharedPreferences(PREFS_FILENAME, 0)
+    }
+
+    fun save() {
+        val gson = Gson()
+
+        val arr = prefs!!.getString(PROFILES, "")
+        //val profiles = gson.fromJson(arr, ArrayList<Profile>::class.java)
+
+        val editor = prefs!!.edit()
+        //editor.putString(PROFILES, gson.toJson(arr))
+        editor.apply()
+
+
+//        SharedPreferences appSharedPrefs = PreferenceManager  .getDefaultSharedPreferences(context.getApplicationContext());
+//     SharedPreferences.Editor prefsEditor = appSharedPrefs.edit();
+//     Gson gson = new Gson();
+//     String json = gson.toJson(tasks); //tasks is an ArrayList instance variable
+//     prefsEditor.putString("currentTasks", json);
+//     prefsEditor.commit();
     }
 
     companion object {
